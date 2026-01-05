e0db71965f3518f34b711c62eb863187ac8ea3fe
git conflict <jigmelhendup33@gmail.com>
2026-01-05 16:21:14 +0530

    Second Commit

--- app/basic.py
+++ app/basic.py
@@ -1,4 +1,4 @@
 print("Hello!")
-x=int(input("Enter a number:"))
-y=int(input("Enter another number:"))
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
 print(f"The sum of {x} and {y} is ",x+y)
